56008dcbe31cf81b9c3f8aacf8bab5ef7d7598ea
Candy Ordoñez Pinto <168691418+Amiddala@users.noreply.github.com>
2026-04-18 17:46:41 -0400

    Add user ID fields to Sesion class

--- ProyectoNetbeans/mavenproject1/Modelo/Sesion.java
+++ ProyectoNetbeans/mavenproject1/Modelo/Sesion.java
@@ -1,21 +1,26 @@
 package com.mycompany.mavenproject1.Modelo;
-
 import java.sql.Connection;
 
 public class Sesion {
     private Connection conexion;
     private int pid;
     private String usuario;
+    private int idUsuario;
+    private int idUserN;
 
-    public Sesion(Connection conexion, int pid, String usuario) {
-        this.conexion = conexion;
-        this.pid      = pid;
-        this.usuario  = usuario;
+    public Sesion(Connection conexion, int pid, String usuario, int idUsuario, int idUserN) {
+        this.conexion  = conexion;
+        this.pid       = pid;
+        this.usuario   = usuario;
+        this.idUsuario = idUsuario;
+        this.idUserN   = idUserN;
     }
 
-    public Connection getConexion() { return conexion; }
-    public int getPid()             { return pid; }
-    public String getUsuario()      { return usuario; }
+    public Connection getConexion()  { return conexion; }
+    public int getPid()              { return pid; }
+    public String getUsuario()       { return usuario; }
+    public int getIdUsuario()        { return idUsuario; }
+    public int getIdUserN()          { return idUserN; } 
 
     public void cerrar() {
         try {
