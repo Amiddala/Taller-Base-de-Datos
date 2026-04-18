0524fcd9a405c8acc1bf7780a962d8a9440e7065
Candy Ordoñez Pinto <168691418+Amiddala@users.noreply.github.com>
2026-04-18 17:48:43 -0400

    Create Archivo class for file management
    Added Archivo class with properties and constructor.

--- ProyectoNetbeans/mavenproject1/Vista/Archivo.java
+++ ProyectoNetbeans/mavenproject1/Vista/Archivo.java
@@ -1,1 +1,43 @@
 
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1.Vista;
+
+/**
+ *
+ * @author candy
+ */
+public class Archivo {
+    private String nombre;
+    private long tamano;       
+    private byte[] contenido;    
+    private int idRepositorio;
+    private int idTipo;
+    private int idFormato;
+    private int idUsuario;
+    private int idEstado;
+
+    public Archivo(String nombre, long tamano, byte[] contenido,
+                   int idRepositorio, int idTipo, int idFormato,
+                   int idUsuario, int idEstado) {
+        this.nombre        = nombre;
+        this.tamano        = tamano;
+        this.contenido     = contenido;
+        this.idRepositorio = idRepositorio;
+        this.idTipo        = idTipo;
+        this.idFormato     = idFormato;
+        this.idUsuario     = idUsuario;
+        this.idEstado      = idEstado;
+    }
+
+    public String getNombre()        { return nombre; }
+    public long getTamano()          { return tamano; }
+    public byte[] getContenido()     { return contenido; }
+    public int getIdRepositorio()    { return idRepositorio; }
+    public int getIdTipo()           { return idTipo; }
+    public int getIdFormato()        { return idFormato; }
+    public int getIdUsuario()        { return idUsuario; }
+    public int getIdEstado()         { return idEstado; }
+}
